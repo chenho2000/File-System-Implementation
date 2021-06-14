200d83dbbc4cda7c0184c7fdca199ff876fcb101
chenho92 <chenho.chen@mail.utoronto.ca>
2021-06-14 18:06:56 -0400

    fix write and read

--- a1fs.c
+++ a1fs.c
@@ -910,7 +910,7 @@ static int a1fs_read(const char *path, char *buf, size_t size, off_t offset,
 	// copy the ans to buf
 	memcpy(buf, ans + offset, size);
 
-	return 0;
+	return size;
 }
 
 /**
@@ -976,7 +976,7 @@ static int a1fs_write(const char *path, const char *buf, size_t size,
 	int start_count;
 	int start_rmd;
 	int extent_idx = 0;
-	struct a1fs_extent *curr_extent;
+	struct a1fs_extent *curr_extent = (a1fs_extent *)(fs->image + file_inode.extent_table * A1FS_BLOCK_SIZE + sizeof(a1fs_extent) * extent_idx);
 	// check where we want to write in
 	while (offset > 0)
 	{
